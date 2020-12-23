4c7e06513d607532cfbca4f8b33d738be5cd2f0f
Ulrich.Gall <gall@innovativeauctions.com>
2020-12-23 06:38:49 -0500

    Changes from SF that hadn't been committed

--- ads1115.py
+++ ads1115.py
@@ -5,7 +5,11 @@ ads = ADS1115.ADS1115()
 
 while True:
     for c in range(4):
-      volt = ads.readADCSingleEnded(channel=c)
+      s = 0
+      for i in range(100):
+        s += ads.readADCSingleEnded(channel=c)
+      volt = s / 100.0
+
       print("{:.0f} mV mesuré sur AN{}".format(volt,c))
     
     time.sleep(0.1)
--- server.py
+++ server.py
@@ -34,191 +34,259 @@ from sklearn import linear_model
 
 
 
+class IccpModuleADS1115:
 
-CIRCUIT_R1 = 0.27 # Ohms; shunt resistor in series to load
+    def __init__(self, 
+        circuit_r1 = 0.27,  # Ohms; shunt resistor in series to load
+        channel_i = 2,
+        channel_v = 3,
+        avg_count = 50,
+        pwm_pin  = 12,
+        ads = None):
+
+        self.circuit_r1 = circuit_r1
+        self.channel_i = channel_i
+        self.channel_v = channel_v
+        self.avg_count = avg_count
+        self.pwm_pin = pwm_pin
+        self.ads = ads
+
+        #initialize PWM
+        GPIO.setmode(GPIO.BCM)
+
+        GPIO.setup(self.pwm_pin, GPIO.OUT)
+        self.pwm = GPIO.PWM(self.pwm_pin, 1200) 
+        self.pwm.start(50.0)
 
 
-# Change this to the local DNS name of your Pi (often raspberrypi.local, if you have changed it) or
-# make it blank to connect to localhost.
-#PI_HOST = 'klabs.local'
+    def read_adc(self,channels):  
+        raw = [0 for ch in channels]
+        for i in range(self.avg_count):
+            for r in range(len(channels)):
+                ch = channels[r]
+                raw[r] = raw[r] + self.ads.readADCSingleEnded(ch)
+        return [x / self.avg_count / 1000.0 for x in raw]
 
-ads = ADS1115.ADS1115()    
+    def read_current(self):
+        u = read_adc([self.channel_i])
+        return(u / self.circuit_r1)
 
-#initialize PWM
-GPIO.setmode(GPIO.BCM)
+    def read_electrode_voltage(self):
+        v0, v1 = self.read_adc([self.channel_i, self.channel_v])
+        return(v1-v0)
 
-pwmpin = 12
-GPIO.setup(pwmpin, GPIO.OUT)
-pwm = GPIO.PWM(pwmpin, 100) 
-pwm.start(50.0)
+    def read_current_and_electrode_voltage(self):
+        v0, v1 =  self.read_adc([self.channel_i, self.channel_v])
+        return [v0 / self.circuit_r1, v1-v0]
 
-def exit_handler():
-    pwm.stop()
-    GPIO.cleanup()
-    print('Stopped PWM, cleaned up GPIO.')
-atexit.register(exit_handler)
+    def set_dc(self,cv_dc):
+        self.cv_dc = cv_dc
+        self.pwm.ChangeDutyCycle(cv_dc)
+
+    def status(self):
+        return("Current = {:.3f} A, Electrode Voltage = {:.3f}V.".format(*self.read_current_and_electrode_voltage()))
+
+    def shutdown(self):
+        self.pwm.stop()
 
 
-class VForDC(threading.Thread) :
+class JobThread(threading.Thread):
+
+    stop_requested = None
+    start_time = None
+    finish_time = None
+
+    def request_stop(self,s):
+        self.stop_requested = s
+
+    def is_still_running(self):
+        return self.start_time != None and self.finish_time == None 
+
+    def run(self):
+        self.start_time = time.time()
+        self.run_job()
+        self.finish_time = time.time()
+
+    def uptime(self):
+        if self.start_time == None:
+            raise(Exception("Not running {}".format(self)))
+        else:
+            return(time.time()-self.start_time)
+
+
+class VForDCThread(JobThread) :
 
     x = None
     ms_v = None
     sample_time = None
-    started = None
-    finished = None
     current_x = 0
 
-    def run(self):
-        if self.started == None:
+    def __init__(self, 
+        iccp = None,
+        sample_time = 0.001):
+        self.iccp = iccp
+        self.sample_time = sample_time
+        super().__init__()
 
-            self.started = time.time()
+    def run_job(self):
+        self.ms_v = []
+        self.x = []
+        for cv_dc in range(0,100,5):
+            print(cv_dc)
+            self.current_x = cv_dc
+            self.x.append(cv_dc)
+            iccp.set_dc(cv_dc)
+            time.sleep(self.sample_time)
+            self.ms_v.append(iccp.read_electrode_voltage())
 
-            self.sample_time = 0.001
-            trace_count = 1
-            self.ms_v = [[] for i in range(trace_count)]
-            self.x = []
-            for cv_dc in range(0,100,1):
-                print(cv_dc)
-                self.current_x = cv_dc
-                self.x.append(cv_dc)
-                pwm.ChangeDutyCycle(cv_dc)
-                for t in range(trace_count):
-                    time.sleep(self.sample_time)
-                    self.ms_v[t].append(read_electrode_voltage())
-            self.finished = time.time()
-        else:
-            print("Won't start again, it's already running for #{}".format(self.started-time.time()))
+# class ICCPThread(JobThread) :
 
-class ICCPController(threading.Thread) :
+#     ms_current = None
+#     ms_voltage = None
+#     cv_voltage = 2.2
+#     cv_dc = 50 # start in the middle
+#     running = False
 
-    ms_current = None
-    ms_voltage = None
-    cv_voltage = 2.2
-    cv_dc = 50 # start in the middle
-    running = False
+#     def __init__(self, 
+#         iccp = None,
+#         sample_time = 0.001, 
 
-    def set_voltage(self,cv_voltage):
-        self.cv_voltage = voltage
+#         self.iccp = iccp
+#         self.sample_time = sample_time
 
-    def run_delta(self):
+#     def set_voltage(self,cv_voltage):
+#         self.cv_voltage = voltage
 
-        print("starting to track...")
+#     def run(self):
 
-        self.running = True
-        pwm.ChangeDutyCycle(self.cv_dc)
+#         print("starting to track...")
 
-        while True:
-            v0, v1 = read_adc(0,1)
-            self.ms_voltage = v1 - v0
-            self.ms_current = v0 / CIRCUIT_R1
-            delta = self.cv_voltage - self.ms_voltage
-            dc = self.cv_dc - 20.0 * delta
-            self.cv_dc = max(min(dc,100),0)
-            print("Commanded voltage is {}. Current measured voltage is {}. Setting dc to {}.".format(self.cv_voltage, self.ms_voltage, self.cv_dc))
-            pwm.ChangeDutyCycle(self.cv_dc)
-            time.sleep(0.010)
+#         self.running = True
+#         iccp.set_dc(self.cv_dc)
 
-def read_adc(channels,avg_count=25):  
-    raw = [0 for ch in channels]
-    for i in range(avg_count):
-        for ch in channels:
-            raw[ch] = raw[ch] + ads.readADCSingleEnded(ch)
-    return [raw[ch] / avg_count / 1000.0 for ch in channels]
+#         while True:
+#             self.ms_voltage, self.ms_current = iccp.read_current_and_electrode_voltage()
+#             delta = self.cv_voltage - self.ms_voltage
+#             dc = self.cv_dc - 20.0 * delta
+#             self.cv_dc = max(min(dc,100),0)
+#             print("Commanded voltage is {}. Current measured voltage is {}. Setting dc to {}.".format(self.cv_voltage, self.ms_voltage, self.cv_dc))
+#             iccp.set_dc(self.cv_dc)
+#             time.sleep(0.010)
 
-def read_current():
-    u = read_adc(0)[0]
-    return(u / CIRCUIT_R1)
 
-def read_electrode_voltage():
-    v0, v1 = read_adc(0,1)
-    return(v1-v0)
+iccp = IccpModuleADS1115(ads=ADS1115.ADS1115())
 
-iccp = ICCPController()
-iccp.start()
-
-v_for_dc = VForDC()
-# v_for_dc.start()
-
+v_for_dc = VForDCThread()
+v_for_dc.start()
 
 @route('/')
 def home():
-    status = "Commanded voltage is {:.3f}V. Current measured voltage is {:.3f}V. Current is {:.3f}A. Implies a resistive load of {:.3f} Ohms. dc was {:.2f}%"
+    # status = "Commanded voltage is {:.3f}V. Current measured voltage is {:.3f}V. Current is {:.3f}A. Implies a resistive load of {:.3f} Ohms. dc was {:.2f}%"
     data = OpenStruct(
-        status = status.format(iccp.cv_voltage, iccp.ms_voltage, iccp.ms_current, iccp.ms_voltage / iccp.ms_current, iccp.cv_dc)
+        status = iccp.status() #status.format(iccp.cv_voltage, iccp.ms_voltage, iccp.ms_current, iccp.ms_voltage / iccp.ms_current, iccp.cv_dc)
         )
     return(template("""
         <h1>{{status}}</h1>
         <h1>Tools</h1>
         <ul>
         <li>
+        <a href="/v_for_dc_regression_plot">Calibration tool - Voltage as a function of duty cycle, regression plot.</a>
         </li>
-        <li>
-        <a href="/time_series?v=2.5">Set voltage to 2.5V</a>
-        </li>
+        <li><a href="/set_electrode_voltage?v=2.5">Set electrode voltage to 2.5V</a></li>
+        <li><a href="/set_dc?dc=50">Set duty cycle to 50%</a></li>
         </ul>
         """,data))
 
 
-@route('/time_series')
-def time_series():
-    iccp.cv_voltage = float(request.query.v)
+# @route('/time_series')
+# def time_series():
+#     iccp.cv_voltage = float(request.query.v)
 
-    sample_time = 0.100
-    ms_v = []
-    cv_dc = []
-    x = []
-    for t in range(100):
-        x.append(t)
-        ms_v.append(iccp.ms_voltage)
-        cv_dc.append(iccp.cv_dc)
-        time.sleep(sample_time)
-    fig = go.Figure()
-    fig.add_trace(go.Scatter(x=x, y=ms_v,
-        mode='lines',
-        name="Electrode Voltage")
-        )
+#     sample_time = 0.100
+#     ms_v = []
+#     cv_dc = []
+#     x = []
+#     for t in range(100):
+#         x.append(t)
+#         ms_v.append(iccp.ms_voltage)
+#         cv_dc.append(iccp.cv_dc)
+#         time.sleep(sample_time)
+#     fig = go.Figure()
+#     fig.add_trace(go.Scatter(x=x, y=ms_v,
+#         mode='lines',
+#         name="Electrode Voltage")
+#         )
 
-    fig.update_layout(title='Voltage as a function of time',
-                   xaxis_title='time (ms)',
-                   yaxis_title='Voltage (V)')
+#     fig.update_layout(title='Voltage as a function of time',
+#                    xaxis_title='time (ms)',
+#                    yaxis_title='Voltage (V)')
 
-    return("<h1>Voltage as a function of time</h1>{}\n".format(fig.to_html()))
+#     return("<h1>Voltage as a function of time</h1>{}\n".format(fig.to_html()))
+
+
+# @route('/set_electrode_voltage')
+# def set_electrode_voltage():
+#     iccp.cv_voltage = float(request.query.v)
+#     iccp.set    
+#     sample_time = 0.100
+#     ms_v = []
+#     cv_dc = []
+#     x = []
+#     for t in range(100):
+#         x.append(t)
+#         ms_v.append(iccp.ms_voltage)
+#         cv_dc.append(iccp.cv_dc)
+#         time.sleep(sample_time)
+#     fig = go.Figure()
+#     fig.add_trace(go.Scatter(x=x, y=ms_v,
+#         mode='lines',
+#         name="Electrode Voltage")
+#         )
+
+#     fig.update_layout(title='Voltage as a function of time',
+#                    xaxis_title='time (ms)',
+#                    yaxis_title='Voltage (V)')
+
+#     return("<h1>Voltage as a function of time</h1>{}\n".format(fig.to_html()))
+
+
+@route('/set_dc')
+def set_dc():
+    dc = float(request.query.dc)
+    iccp.set_dc(dc)
+    return("<h1>Duty Cycle set to {:.2f}%.</h1>\n<p>{}</p><p><a href=\"/\">Back to main page.</a></p>".format(dc,iccp.status()))
 
 
 @route('/v_for_dc_regression_plot')
 def v_for_dc_regression_plot():
-
-    if (v_for_dc.started == None):
-        v_for_dc.started = time.time()
+    global v_for_dc
+    if (v_for_dc.start_time == None):
         v_for_dc.start()
+        return "Started Calibration. Please reload to see results."
 
-    if (v_for_dc.started != None and v_for_dc.finished == None ):
-        return("<h1>Already running</h1><p>Not finished yet, have {} points after {}s. </p>".format(v_for_dc.current_x,time.time() - v_for_dc.started))
+    if (v_for_dc.is_still_running()):
+        return("<h1>Already running</h1><p>Not finished yet, have {} points after {:.3f}s. </p>".format(v_for_dc.current_x,time.time() - v_for_dc.start_time))
 
-    npy = np.array(v_for_dc.ms_v[0])
+    npy = np.array(v_for_dc.ms_v)
     npx = np.array(v_for_dc.x)
     lm=LinearRegression()
     lm.fit(npx.reshape(-1,1),npy.reshape(-1,1))
     res=lm.predict(npx.reshape(-1,1)).reshape(1,-1)
 
-    fig = go.Figure()
-    for t in range(len(v_for_dc.ms_v)):
-        fig.add_trace(go.Scatter(x=v_for_dc.x, y=v_for_dc.ms_v[t],
-            mode='lines',
-            name="Electrode Voltage with #{:.3f}s delay".format(t*v_for_dc.sample_time)
-            ))
+    print(res)
 
-  
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=v_for_dc.x, y=v_for_dc.ms_v,
+        mode='lines',
+        name="Electrode Voltage"))
     fig.add_trace(go.Scatter(x=v_for_dc.x, y=res[0],
         mode='lines',
-        name="Regression fit".format(t*v_for_dc.sample_time)))
-
+        name="Regression fit"))
     fig.update_layout(title='Voltage as a function of duty cycle',
                    xaxis_title='Duty Cycle (%)',
                    yaxis_title='Voltage (V)')
-
+    v_for_dc = VForDCThread()
     return("<h1>Voltage as a function of duty cycle</h1>{}\n".format(fig.to_html()))
-
 
 
 run(host='localhost', port=8080, debug=True)
@@ -228,4 +296,10 @@ run(host='localhost', port=8080, debug=True)
 # with open(r'name', 'a') as f:
 #     writer = csv.writer(f)
 #     writer.writerow(fields)
+
+def exit_handler():
+    iccp.shutdown()
+    GPIO.cleanup()
+    print('Stopped iccp, cleaned up GPIO.')
+atexit.register(exit_handler)
 
